e3ec186ada7e178635c63bb0637566ac3382c35b
vadkutsen <vadkutsen@gmail.com>
2022-09-21 11:35:03 +0200

    Rename currency in the currencyId file

--- interface/src/utils/currencyId.ts
+++ interface/src/utils/currencyId.ts
@@ -1,7 +1,7 @@
 import { Currency, OBX, Token } from 'obiswap'
 
 export function currencyId(currency: Currency): string {
-  if (currency === OBX) return 'ETH'
+  if (currency === OBX) return 'OBX'
   if (currency instanceof Token) return currency.address
   throw new Error('invalid currency')
 }
